d53592f2c3522e8749cfeab093653a8689cd71f5
Lohith Bollineni <bollineni.lohith9@gmail.com>
2022-08-29 02:26:49 -0400

    dsafirst

---
+++ algos1.py
@@ -0,0 +1,11 @@
+# Algorithm ( Statement, Input, Output )
+
+# Linear/Sequential/Simple Search
+
+# Efficiency ( Time & Space Complexity )
+
+# Big O / O(n) : Complexity of Algorithm
+
+# Upper Bounds : Worst Case
+
+# O(1): Constant, O(log n): Sublinear, O(n): Linear, O(n^2): Quadratic, O(n^3): Cubic, O(n*log n): Quasilinear, O(n^k): Polynomial
